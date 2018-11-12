fc2ae2c4268f977938fb4e43a722bec39f1418dc
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-11-12 18:38:24 +0100

    Application success page

---
+++ src/app/application/application-success/application-success.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { UserApplicationService } from '../../services/application-service/user-application.service';
+
+@Component({
+  selector: 'app-application-success',
+  templateUrl: './application-success.component.html',
+  styleUrls: ['./application-success.component.scss']
+})
+export class ApplicationSuccessComponent implements OnInit {
+
+  candidateFirstName:string;
+
+  constructor(private userApplicationService: UserApplicationService) { }
+
+  ngOnInit() {
+    this.candidateFirstName = this.userApplicationService.getCandidateFirstName();
+  }
+}
