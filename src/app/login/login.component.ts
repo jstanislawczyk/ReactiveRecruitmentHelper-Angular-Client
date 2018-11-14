6b184cbbc276f0173a95dc95362f088f41cd765e
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-11-14 22:03:59 +0100

    Created login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  
+  loginForm:FormGroup;
+  loginFailed:boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    this.loginForm = this.createApplicationForm();
+  }
+
+  createApplicationForm() {
+    return  this.formBuilder.group({ 
+      login: [''],
+      password: ['']
+    })
   }
 
   login() {
-
+    
   }
 }
