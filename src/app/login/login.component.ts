e4f768d15e5d94b8e7025736965c8cf2862b23d3
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-11-16 15:16:41 +0100

    Login component connection with auth service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication-service/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,9 @@ export class LoginComponent implements OnInit {
   loginForm:FormGroup;
   loginFailed:boolean = false;
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(
+    private formBuilder:FormBuilder,
+    private authenticationService:AuthenticationService) { }
 
   ngOnInit() { 
     this.loginForm = this.createApplicationForm();
@@ -25,6 +28,9 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    
+    const login = this.loginForm.controls.login.value;
+    const password = this.loginForm.controls.password.value;  
+
+    this.authenticationService.authenticateUser(login, password);
   }
 }
