f783f4fd4e248da9134309b812afbc2c2062f39e
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-11-20 19:45:47 +0100

    Updated User class

--- src/app/classes/User.ts
+++ src/app/classes/User.ts
@@ -1,57 +1,35 @@
+import {Role} from './Role';
+
 class User {
-    private id: string;
-    private firstName: string;
-    private lastName: string;
-    private email: string;
-    private password: string;
-
-    constructor(id: string, firstName: string, lastName: string, email: string, password: string) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
+    get email(): string {
+      return this._email;
+    }
+    get firstName(): string {
+      return this._firstName;
+    }
+    get lastName(): string {
+      return this._lastName;
     }
 
-    getId() {
-        return this.id;
+    get roles(): Array<Role> {
+      return this._roles;
     }
 
-    getFirstName() {
-        return this.firstName;
-    }
+    private _id: string;
+    private readonly _firstName: string;
+    private readonly _lastName: string;
+    private readonly _email: string;
+    private readonly _password: string;
+    private readonly _roles: Array<Role> = [];
 
-    getLastName() {
-        return this.lastName;
-    }
-
-    getEmail() {
-        return this.email;
-    }
-
-    getPassword() {
-        return this.password;
-    }
-
-    setId(id: string) {
-        return this.id = id;
-    }
-
-    setFirstName(firstName: string) {
-        return this.firstName = firstName;
-    }
-
-    setLastName(lastName: string) {
-        return this.lastName = lastName;
-    }
-
-    setEmail(email: string) {
-        return this.email = email;
-    }
-
-    setPassword(password: string) {
-        return this.password = password;
+    constructor(id: string, firstName: string, lastName: string, email: string, password: string, roles: Array<Role>) {
+        this._id = id;
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._email = email;
+        this._password = password;
+        this._roles = roles;
     }
 }
 
-export {User}
+export {User};
