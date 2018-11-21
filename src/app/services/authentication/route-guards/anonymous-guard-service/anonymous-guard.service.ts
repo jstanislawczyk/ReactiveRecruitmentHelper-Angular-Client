fd0c6e6c9f4c8d81dab1827790fd286814635279
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-11-21 13:29:31 +0100

    New anonymous guard service

---
+++ src/app/services/authentication/route-guards/anonymous-guard-service/anonymous-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { AuthenticationService } from '../../authentication-service/authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnonymousGuardService implements CanActivate {
+
+  constructor(public authenticationService: AuthenticationService, public router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authenticationService.isUserAuthenticated) {
+      this.router.navigate(['']).then();
+      return false;
+    }
+    return true;
+  }
+}
