63c080f5825879ae0dbf6720315ede69358fe238
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-11-22 22:01:56 +0100

    Constructor for Role class

--- src/app/classes/Role.ts
+++ src/app/classes/Role.ts
@@ -1,9 +1,14 @@
 class Role {
+
   get authority(): string {
     return this._authority;
   }
 
   private readonly _authority: string;
+
+  constructor(authority: string) {
+    this._authority = authority;
+  }
 }
 
 export {Role};
