25c5f7b1080cef701324d525123276bba1409bf7
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-11-23 14:22:40 +0100

    Simple validation for text fields

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Role} from '../classes/Role';
 
 @Component({
@@ -8,11 +9,15 @@ import {Role} from '../classes/Role';
 })
 export class UsersComponent implements OnInit {
 
+  usersCreateForm: FormGroup;
   isCaptchaNotResolved = true;
   rolesList = UsersComponent.setupRoles();
   rolesCheckStatusList = this.setupCheckedRolesList();
+  isFormSubmitted = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   private static setupRoles() {
     return [
@@ -25,7 +30,26 @@ export class UsersComponent implements OnInit {
     return new Array(this.rolesList.length).fill(false);
   }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.usersCreateForm = this.createUsersCreateForm();
+  }
+
+  createUsersCreateForm(): FormGroup {
+    return this.formBuilder.group({
+      email: [
+        '', [Validators.required, Validators.email]
+      ],
+      password: [
+        '', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]
+      ],
+      firstName: [
+        '', [Validators.required, Validators.minLength(3), Validators.maxLength(60)]
+      ],
+      lastName: [
+        '', [Validators.required, Validators.minLength(3), Validators.maxLength(80)]
+      ]
+    });
+  }
 
   captchaResolved(): void {
     this.isCaptchaNotResolved = false;
@@ -33,5 +57,9 @@ export class UsersComponent implements OnInit {
 
   changeStyleOnCheckStatusChange(index: number) {
     this.rolesCheckStatusList[index] = !this.rolesCheckStatusList[index];
+  }
+
+  createUser() {
+    this.isFormSubmitted = true;
   }
 }
