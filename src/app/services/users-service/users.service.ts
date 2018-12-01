7057f29f111bdf4a669400440ddb9c47973bb575
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-12-01 21:29:27 +0100

    Get users list from REST service

--- src/app/services/users-service/users.service.ts
+++ src/app/services/users-service/users.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthenticationService } from '../authentication/authentication-service/authentication.service';
+import {User} from '../../classes/User';
 
 @Injectable({
   providedIn: 'root'
@@ -6,10 +9,30 @@ import { Injectable } from '@angular/core';
 export class UsersService {
 
   private usersServiceUri = 'http://localhost:8090/users';
+  private usersList: Array<User>;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private authenticationService: AuthenticationService
+  ) { }
 
-  findUsers() {
-    console.log(this.usersServiceUri);
+  findUsers(): void {
+    const header = this.createHeader();
+
+    this.http
+      .get(this.usersServiceUri, header)
+      .subscribe(
+        (result) => console.log(result)
+      );
+  }
+
+  private createHeader(): Object {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Basic ${this.authenticationService.userAuthenticationToken}`
+      })
+    };
   }
 }
