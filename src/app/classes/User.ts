b1bb23cc2d7b14509aa23b2a120aa063c2679add
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-12-03 21:10:31 +0100

    User class updated with _id getter

--- src/app/classes/User.ts
+++ src/app/classes/User.ts
@@ -1,35 +1,41 @@
 import { Role } from './Role';
 
 class User {
-    get email(): string {
-      return this._email;
-    }
-    get firstName(): string {
-      return this._firstName;
-    }
-    get lastName(): string {
-      return this._lastName;
-    }
+  get _id(): string {
+    return this.__id;
+  }
 
-    get roles(): Array<Role> {
-      return this._roles;
-    }
+  get email(): string {
+    return this._email;
+  }
 
-    private readonly _id: string;
-    private readonly _firstName: string;
-    private readonly _lastName: string;
-    private readonly _email: string;
-    private readonly _password: string;
-    private readonly _roles: Array<Role> = [];
+  get firstName(): string {
+    return this._firstName;
+  }
 
-    constructor(id: string, firstName: string, lastName: string, email: string, password: string, roles: Array<Role>) {
-        this._id = id;
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this._email = email;
-        this._password = password;
-        this._roles = roles;
-    }
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  get roles(): Array<Role> {
+    return this._roles;
+  }
+
+  private readonly __id: string;
+  private readonly _firstName: string;
+  private readonly _lastName: string;
+  private readonly _email: string;
+  private readonly _password: string;
+  private readonly _roles: Array<Role> = [];
+
+  constructor(id: string, firstName: string, lastName: string, email: string, password: string, roles: Array<Role>) {
+      this.__id = id;
+      this._firstName = firstName;
+      this._lastName = lastName;
+      this._email = email;
+      this._password = password;
+      this._roles = roles;
+  }
 }
 
 export { User };
